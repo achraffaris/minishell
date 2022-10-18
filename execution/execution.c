cce6c1792465e5516f5c0d81010f6febc63086dc
Achraf Faris <afaris@student.1337.ma>
2022-10-18 10:27:42 +0000

    exec stuct needs to be updated

--- execution/execution.c
+++ execution/execution.c
@@ -1,17 +1,46 @@
 #include "execution.h"
 
+int     exit_status;
 
+void    run_cmd(t_parse *cmd)
+{
+    if (run_as_builtin(cmd))
+        return ;
+}
+
+t_exec  *setup_exec(t_parse *cmds)
+{
+    t_exec  *exe;
+    t_parse *current;
+
+    exe = malloc(sizeof(t_exec));
+    if (!exe)
+        raise_error("Memory allocation failed!", "malloc");
+    exe->cmds = cmds;
+    exe->ncmds = 0;
+    current = cmds;
+    while (current)
+    {
+        exe->ncmds++;
+        current = current->next;
+    }
+    exe->pipes = malloc(sizeof(int *) * (exe->ncmds - 1));
+    if (!exe->pipes)
+        raise_error("Memory allocation failed!", "malloc");
+    return (exe);
+}
 
 void    execution(t_parse *data)
 {
+    int      pid;
+    t_exec  *exe;
     t_parse *current;
 
-    current = data;
+    exe = setup_exec(data);
+    current = exe->cmds;
     while (current)
     {
-        if (run_as_builtin(current))
-            current = current->next;
-        else
-
+        run_cmd(current);
+        current = current->next;
     }
 }
--- execution/execution.h
+++ execution/execution.h
@@ -3,22 +3,26 @@
 
 #include "../header.h"
 #include "builtins/builtins.h"
+#include <sys/types.h>
+#include <sys/wait.h>
+
 #define ERROR_RETURNED -1
 #define AFTER 1
 #define BEFORE 0
 #define NONE -1
+
+typedef struct s_execution
+{
+    t_parse     *cmds;
+    int         **pipes;
+    int         ncmds;
+}   t_exec;
+
 
 void    execution(t_parse *data);
 void    raise_error(char *err_msg, char *err_src);
 int     substring_length(char *str, char to_stop, int mode);
 t_env   *setup_env(char **env);
 
-
-typdef struct execution
-{
-    t_parse *cmds;
-    int     **pipes;
-    int     ncmds;
-}   t_exec;
 
 #endif
